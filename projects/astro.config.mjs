5f184f72bc3d1813604f0ea7c5ea798e82749ccb
Kelvin Lin <kelvindlin97@gmail.com>
2024-10-10 21:56:50 -0400

    fixing

--- projects/astro.config.mjs
+++ projects/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	outDir: 'dist',
 	site: 'https://kelvinlin.github.io',
 	integrations: [mdx(), sitemap()],
 	base: 'personal-portfolio',
