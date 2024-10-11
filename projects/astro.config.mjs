9f0abc80dbf90e436171095407a386f7bffb341c
Kelvin Lin <kelvindlin97@gmail.com>
2024-10-10 22:05:30 -0400

    updates

--- projects/astro.config.mjs
+++ projects/astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import github from '@astrojs/github';
 
 // https://astro.build/config
 export default defineConfig({
-	outDir: 'dist',
-	site: 'https://kelvinlin.github.io',
-	integrations: [mdx(), sitemap()],
-	base: 'personal-portfolio',
-});
+	output: 'static',
+	integrations: [github()],
+	// Add your base path if your repository is not your GitHub username
+	base: '/personal-portfolio', // Replace 'your-repo-name' with your GitHub repository name
+  });
+  
